d403d8b1813aa2336209aeb2266dcd95f2a9cb4d
Scot Spinner <scot.spinner@gmail.com>
2014-12-12 10:17:02 -0800

    adding bundler exec for including gems

--- cron_wrapper.sh
+++ cron_wrapper.sh
@@ -2,6 +2,6 @@
 
 
 export PATH=/opt/rbenv/shims:/opt/rbenv/bin:/usr/bin:$PATH
-export GEM_HOME=/opt/cronmon/vendor/bundler/ruby/2.0.0/gems
 eval "$(rbenv init -)"
-cd /opt/cronmon && ruby cron_wrapper.rb $1
+
+cd /opt/cronmon && bundle exec ruby ./cron_wrapper.rb $1
